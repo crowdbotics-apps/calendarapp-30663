49c31cfea9894a2704c01a7aba24d2546cb073db
Crowdbotics <team@crowdbotics.com>
2021-09-18 20:12:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "calendarapp_30663Identifier",
+  url: "https://calendarapp_30663Identifier.botics.co",
+  api: "https://calendarapp_30663Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
